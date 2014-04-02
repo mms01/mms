c0f2797ed11bafc591d80f3219f87eb7c41f4e63
unknown <P1450543@P1450543A.ad.ogis-ri.co.jp>
2014-04-02 19:25:03 +0900

    project_20140402

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140328134927) do
+ActiveRecord::Schema.define(:version => 20140323144340) do
 
   create_table "meetings", :force => true do |t|
     t.string   "title"
