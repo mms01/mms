f53cefc378af13690d25cf4419192e27d46fb296
morii <morii_miwa@ogis-ri.co.jp>
2014-07-17 16:13:51 +0900

    Projectにavailableを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(:version => 20140708054920) do
-=======
-ActiveRecord::Schema.define(:version => 20140708045122) do
->>>>>>> 807eda264ce8c538d90130948fe7361de47eea5e
+ActiveRecord::Schema.define(:version => 20140717061132) do
 
   create_table "meetings", :force => true do |t|
     t.string   "title"
@@ -53,8 +49,8 @@ ActiveRecord::Schema.define(:version => 20140708045122) do
 
   create_table "projects", :force => true do |t|
     t.string   "name"
-    t.datetime "created_at",     :null => false
-    t.datetime "updated_at",     :null => false
+    t.datetime "created_at",                       :null => false
+    t.datetime "updated_at",                       :null => false
     t.string   "manager_id"
     t.string   "create_user_id"
     t.date     "start_date"
@@ -62,6 +58,7 @@ ActiveRecord::Schema.define(:version => 20140708045122) do
     t.text     "purpose"
     t.string   "sts"
     t.binary   "attached"
+    t.boolean  "available",      :default => true
   end
 
   create_table "todos", :force => true do |t|
@@ -78,21 +75,21 @@ ActiveRecord::Schema.define(:version => 20140708045122) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "", :null => false
-    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "email",                  :default => "",   :null => false
+    t.string   "encrypted_password",     :default => "",   :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          :default => 0,  :null => false
+    t.integer  "sign_in_count",          :default => 0,    :null => false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                             :null => false
-    t.datetime "updated_at",                             :null => false
+    t.datetime "created_at",                               :null => false
+    t.datetime "updated_at",                               :null => false
     t.string   "role"
     t.string   "user_name"
-    t.boolean  "available"
+    t.boolean  "available",              :default => true
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
